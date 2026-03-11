a34754d70b3d4351624dc032b3c0d788d312a895
Farrassubahan <farrasoficial@gmail.com>
2026-03-11 10:13:18 +0700

    setup css,js & font

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    You are logged in!
+                    <div class="panel-body">
+                        You are logged in!
+                        <button class="btn btn-primary">
+                            <i class="fa fa-plus"></i> Tambah Buku
+                        </button>
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,87 +1,163 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Mobile Responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
+    <!-- CSRF -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'L Farpus') }}</title>
 
-    <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
-    <!-- Scripts --> 
-    <script>
-        window.Laravel = <?php echo json_encode([
-            'csrfToken' => csrf_token(),
-        ]); ?>
-    </script>
+    <!-- Custom Style -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> --}}
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+
 </head>
+
 <body>
+
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
+
+        <nav class="navbar navbar-inverse navbar-fixed-top">
             <div class="container">
+
+                <!-- Brand and toggle -->
                 <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#main-navbar">
+
+                        <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
+
                     </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
+                    <a class="navbar-brand" href="{{ url('/home') }}">
                         {{ config('app.name', 'L Farpus') }}
                     </a>
+
                 </div>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
+                <!-- Navbar Menu -->
+                <div class="collapse navbar-collapse" id="main-navbar">
+
+                    <!-- LEFT MENU -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+
+                        <li class="active">
+                            <a href="{{ url('/') }}">
+                                <span class="glyphicon glyphicon-home fa fa-home"></span>
+                                Beranda
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <span class="glyphicon glyphicon-book fa fa-book"></span>
+                                Buku
+                            </a>
+                        </li>
+
                     </ul>
 
-                    <!-- Right Side Of Navbar -->
+
+                    <!-- RIGHT MENU -->
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
+
                         @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
+                            <li>
+                                <a href="{{ url('/login') }}">
+                                    <span class="glyphicon glyphicon-log-in"></span>
+                                    Login
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="{{ url('/register') }}">
+                                    <span class="glyphicon glyphicon-user"></span>
+                                    Daftar
+                                </a>
+                            </li>
                         @else
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
+                                    <span class="glyphicon glyphicon-user"></span>
+                                    {{ Auth::user()->name }}
+                                    <span class="caret"></span>
+
                                 </a>
 
-                                <ul class="dropdown-menu" role="menu">
+                                <ul class="dropdown-menu">
+
                                     <li>
                                         <a href="{{ url('/logout') }}"
                                             onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                   document.getElementById('logout-form').submit();">
+
+                                            <span class="glyphicon glyphicon-log-out"></span>
                                             Logout
                                         </a>
 
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST"
+                                            style="display:none">
+
                                             {{ csrf_field() }}
+
                                         </form>
+
                                     </li>
+
                                 </ul>
+
                             </li>
                         @endif
+
                     </ul>
+
                 </div>
+
             </div>
         </nav>
 
-        @yield('content')
+        <!-- CONTENT -->
+        <div class="container" style="margin-top:80px">
+
+            @yield('content')
+
+        </div>
+
+        <footer class="navbar navbar-inverse navbar-static-bottom">
+            <div class="container text-center">
+                <p class="navbar-text">
+                    © {{ date('Y') }} {{ config('app.name', 'L Farpus') }} — Sistem Perpustakaan
+                </p>
+            </div>
+        </footer>
+
+
     </div>
 
-    <!-- Scripts -->
-    <script src="/js/app.js"></script>
+
+    <!-- JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">\
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+
 </body>
+
 </html>
